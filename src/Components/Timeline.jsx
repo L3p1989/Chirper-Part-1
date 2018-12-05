cbf8ceef2c561d0cca232970204382a4245bd99c
Patrick Hogan <phogan110@live.com>
2018-12-05 16:13:49 -0700

    created `Timeline`

---
+++ src/Components/Timeline.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Timeline = () => {
+  return (
+    <div className="card w-25 text-center m-2">
+      <div className="card-body">
+        <h4 className="card-title">Username</h4>
+        <p className="card-text">chirpity chirp</p>
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
