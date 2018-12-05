881ada01b840124ad47b78c6c15b57d9b76f8aae
Patrick Hogan <phogan110@live.com>
2018-12-05 11:15:10 -0700

    changed from js to jsx

---
+++ src/Components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="jumbotron text-center m-2">Jumbotron!</div>;
+  }
+}
+
+export default App;
