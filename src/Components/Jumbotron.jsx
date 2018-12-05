47157016a16a0d38c74a6a958b41ca7fd599490e
Patrick Hogan <phogan110@live.com>
2018-12-05 13:49:02 -0700

    replaced `Jumbotron` with `NavBar`

--- src/Components/Jumbotron.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Jumbotron extends Component {
-  render() {
-    return <div className="jumbotron text-center m-2">Jumbotron!</div>;
-  }
-}
-
-export default Jumbotron;
