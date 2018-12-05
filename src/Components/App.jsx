4cb583780d31ed9b91473bc929a8ad7484d70303
Patrick Hogan <phogan110@live.com>
2018-12-05 14:48:47 -0700

    Added `form` for name and text

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,6 +6,30 @@ class App extends Component {
     return (
       <>
         <NavBar />
+        <div className="container align-content-center mt-5">
+          <div className="row">
+            <div className="col-md-12">
+              <form className="border border-dark">
+                <div className="form-group m-5">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail"
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="form-group m-5">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail"
+                    placeholder="What's on your mind?"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
