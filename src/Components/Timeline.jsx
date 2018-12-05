487527d836b2211248c1bbc3c888259245e0a008
Patrick Hogan <phogan110@live.com>
2018-12-05 16:37:06 -0700

    moved card into `map()` to insert `userName` and `chirp` into card

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-const Timeline = () => {
+const Timeline = props => {
   return (
-    <div className="card w-25 text-center m-2">
-      <div className="card-body">
-        <h4 className="card-title">Username</h4>
-        <p className="card-text">chirpity chirp</p>
-      </div>
+    <div className="row">
+      {props.chirps.map((chirp, index) => {
+        return (
+          <div key={index} className="card w-25 text-center m-2 rounded">
+            <div className="card-body">
+              <h4 className="card-title">{chirp.userName}</h4>
+              <p className="card-text">{chirp.chirp}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
