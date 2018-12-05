2c9055e5b0241cc6e5fe39b6b4a77e296b84d9a3
Patrick Hogan <phogan110@live.com>
2018-12-05 11:40:58 -0700

    created `Jumbotron` component

---
+++ src/Components/Jumbotron.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Jumbotron extends Component {
+  render() {
+    return <div className="jumbotron text-center m-2">Jumbotron!</div>;
+  }
+}
+
+export default Jumbotron;
